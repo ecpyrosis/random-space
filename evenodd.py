c7e61df9de49d3a9a782cc3e2fdbad19da88718c
ecpyrosis <gpeters@apple.com>
2021-02-13 21:03:07 -0800

    so awesome

--- evenodd.py
+++ evenodd.py
@@ -3,28 +3,33 @@
 # odd or even
 
 cont = 'yes'
-while (cont == 'yes') or (cont has 'y') :
+while (cont == 'yes' or cont == 'y'):
+
     print('Give a number between 1 and 1000, and I will tell you if it is even or odd')
     num = input('What number are you thinking?\nInput: ')
-    numIn = int(num)
 
-    if (numIn < 1 or numIn > 1000):
-        print('That is not a number between 1 and 1000, bye!')
-        break
+    try:
+        numIn = int(num)
+        if (numIn < 1 or numIn > 1000):
+            print('That is not a number between 1 and 1000, bye!')
+            break
 
-    elif numIn % 2 == 0:
-        print("That's an even number!")
-        a = input('Have another?')
-        if a == cont:
-            continue 
+        elif numIn % 2 == 0:
+            print("That's an even number!")
+            a = input('Have another?')
+            if (a == 'yes' or a == 'y'):
+                continue 
+            else:
+                print('ok bye')
+                break
         else:
-            print('ok bye')
-            break
-    else:
-        print("That's an odd number!")
-        a = input('Have another?')
-        if a == cont:
-            continue 
-        else:
-            print('ok bye')
-            break
+            print("That's an odd number!")
+            a = input('Have another?')
+            if (a == 'yes' or a =='y'):
+                continue 
+            else:
+                print('ok bye')
+                break
+    except ValueError:
+        numIn = int(num)
+        print('That is not a number')
