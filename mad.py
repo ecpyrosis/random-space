55573320974614f2ac4dbd64a5e66226bbae6e60
ecpyrosis <gpeters@apple.com>
2021-02-16 20:39:18 -0800

    more awesome

---
+++ mad.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+rounds = 0
+
+while rounds < 3:
+
+    noun = input('Enter a noun\n')
+    adj = input('Enter an adjective\n')
+    adv = input('Enter an adverb\n')
+    exl = input('Enter an exclamation\n')
+
+    print(exl + '! he said '+ adv + ' as he jumped into his convertable ' + noun + ' and drove off with his '+ adj + ' wife.')
+    rounds += 1
