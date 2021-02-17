bd7262617dfa430b390c7c223172fda86f06b79f
ecpyrosis <gpeters@apple.com>
2021-02-16 21:58:14 -0800

    much goodness

---
+++ whatsup.py
@@ -0,0 +1,22 @@
+#!/usr/env pyhon
+
+count = 0
+
+str1 = (input("What's on your mind today?\n"))
+
+for i in str1.split():
+    count += 1
+
+# print(str1.split())
+
+print('That was ' + str(count) + ' words long!\n')
+
+r = open('foo')
+myText = r.read()
+myWords = myText.split()
+r.close()
+
+print('Counting words in your file...')
+print('wow! that was ' + str(len(myWords)) + ' words long in your file')
+
+
