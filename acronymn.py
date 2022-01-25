eacd429e374d60e0dcead00081391cb4af41db1c
ecpyrosis <78231172+ecpyrosis@users.noreply.github.com>
2022-01-25 10:13:24 -0800

    break out the print to see under the hood

--- acronymn.py
+++ acronymn.py
@@ -6,4 +6,6 @@ phrase = (user_input.replace('of', '').replace('without', 'with out').split())
 acro = ""
 for i in phrase:
     acro = acro + i[0].upper()
+print('From the phrase:  {}'.format(user_input))
+print('We converted it like so: {}'.format(phrase))
 print('Your acronym is: {}'.format(acro))
