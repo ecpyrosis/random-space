c5a5df1052c7a1ad850e0c82891ba7321153e8ed
ecpyrosis <78231172+ecpyrosis@users.noreply.github.com>
2022-01-28 13:11:33 -0800

    quite possibly the greatest addition to python

--- getPage.py
+++ getPage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 # grab first page of a website
+# suddenly easier thanks to https://docs.python-requests.org/en/master/
 
 import requests
 
 r = requests.get('https://www.disney.com')
 print(r.content)
+# print(r.headers)
+# print(r.elapsed)
