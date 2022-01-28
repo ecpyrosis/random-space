805979c3acb43c600e570a043fde8d6ebc2d12b7
ecpyrosis <78231172+ecpyrosis@users.noreply.github.com>
2022-01-28 15:36:14 -0800

    more stuffs of awesomeness

--- getPage.py
+++ getPage.py
@@ -3,18 +3,15 @@
 # grab first page of a website
 # suddenly easier thanks to https://docs.python-requests.org/en/master/
 
-
 # import urllib3
-
 # http = urllib3.PoolManager()
-
 # r = http.request("GET", "https://www.disney.com")
 # print(r.data)
-
 
 import requests
 
 r = requests.get('https://www.disney.com')
-print(r.content)
+# print(r.content)
+print(r.text)
 # print(r.headers)
 # print(r.elapsed)
--- getPageHard.py
+++ getPageHard.py
@@ -2,9 +2,10 @@
 
 # get a page the hard way
 
-import urllib3
+# import urllib3
+import urllib
 
-http = urllib3.PoolManager()
+# http = urllib3.PoolManager()
 
 r = http.request("GET", "https://www.disney.com")
 print(r.data)
