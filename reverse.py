1cc9149747c570a9924891ec11e50091970a759e
ecpyrosis <78231172+ecpyrosis@users.noreply.github.com>
2022-02-01 13:42:27 -0800

    More glorious additions

---
+++ reverse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# def reverse_string(word):
+#     for i in word[::-1]:
+#         print(i)
+# reverse_string('pooch')
+
+
+# def reverse_string(word):
+#     r = word[::-1]
+#     print(r)
+
+def reverse_string(word):
+     print(word[::-1])
+
+reverse_string("Here's an example")
+reverse_string('pooch')
+
+def reverse_words_in_sentence(sentence):
+    sentence.split()
+    print(sentence[::-1])
+
+reverse_words_in_sentence("here is a sentence")
+
