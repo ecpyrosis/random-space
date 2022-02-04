f78956c9c5ed5eb1ba495e77ff90aed4c58f6853
ecpyrosis <78231172+ecpyrosis@users.noreply.github.com>
2022-02-03 16:49:12 -0800

    finally, list comprehension to the rescue

--- reverse.py
+++ reverse.py
@@ -10,16 +10,56 @@
 #     r = word[::-1]
 #     print(r)
 
+# print(str(new_string))
+
+# print(reverse_string("Here's an example"))
+# print(reverse_string('pooch'))
+
+# def reverse_words_in_sentence(sentence):
+#     sentence.split()
+#     for i in (sentence[::-1]):
+#         print(i)
+
+# mystring = "Santa Cruz is cool"
+# new_string = mystring.split()
+# list.reverse(new_string)
+# print(new_string)
+
+# Python code to Reverse each word
+# of a Sentence individually
+
+# Function to Reverse words
+# def reverseWordSentence(Sentence):
+#     # Splitting the Sentence into list of words.
+#     words = Sentence.split(" ")
+#
+#     # Reversing each word and creating
+#     # a new list of words
+#     # List Comprehension Technique
+#     newWords = [word[::-1] for word in words]
+#
+#     # Joining the new list of words
+#     # to for a new Sentence
+#     newSentence = " ".join(newWords)
+#
+#     return newSentence
+
+# reverse a string
 def reverse_string(word):
-     print(word[::-1])
+    return(word[::-1])
 
-reverse_string("Here's an example")
-reverse_string('pooch')
-
-# uh, these seem to be identical, do not need to use split()
+# reverse words in a sentence
 def reverse_words_in_sentence(sentence):
-    # sentence.split()
-    print(sentence[::-1])
+    words = sentence.split(" ")
+    newWords = [word[::-1] for word in words]
+    newSentence = " ".join(newWords)
+    return newSentence
 
-reverse_words_in_sentence("here is a sentence")
+
+print(reverse_string("Here's") == "s'ereH")
+print(reverse_words_in_sentence("This is an example"))
+
+# simple way to test
+print(reverse_words_in_sentence("This is an example") == "sihT si na elpmaxe")
+print(reverse_words_in_sentence("Here's another sentence") == "s'ereH rehtona ecnetnes")
 
