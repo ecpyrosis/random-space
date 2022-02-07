dd21b1be35e3434bb33686db9c3fdca54dfdcbc3
ecpyrosis <78231172+ecpyrosis@users.noreply.github.com>
2022-02-07 13:57:50 -0800

    fix it up

--- numberguess.py
+++ numberguess.py
@@ -3,18 +3,18 @@
 import random
 
 number = random.randint(1,50)
-print(number)
-guess = int(input("Guess a number between 1 and 50: "))
+guess = int(input("Guess a number between 1 and 50: \n"))
 tries = 0
+
 while guess != number:
     if guess < number:
         tries += 1
         print("Too cold. Try again")
-        guess = int(input("Guess a number between 1 and 50: "))
+        guess = int(input("Guess a number between 1 and 50: \n"))
     else:
         tries += 1
         print("Too hot. Try again")
-        guess = int(input("Guess a number between 1 and 50: "))
+        guess = int(input("Guess a number between 1 and 50: \n"))
 
 tries += 1
 print("You guessed correctly! It took you " + str(tries) + " tries" )
