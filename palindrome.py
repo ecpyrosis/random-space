940f80bd2834152534815036998f14f12e46daf1
ecpyrosis <78231172+ecpyrosis@users.noreply.github.com>
2022-02-17 09:58:22 -0800

    palindromic

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,23 @@
 #! /usr/bin/env python
 
-words = input("Enter a word to see if it is a palindrome): \n")
+wordlist = [item for item in input("Enter a word to see if it is a palindrome): ").split()]
+print(wordlist)
 
-def ispalindrome(words):
-    forwardlist = words
-    reversedList = forwardlist[::-1]
+def ispalindrome(i):
+
+    reversedList = wordlist[::-1]
+
     # reversedList = [forwardList[::-1] for forwardlist in words]
-    if reversedList == forwardlist:
-        return(words + " is a palindrome")
+    # if reversedList == forwardlist:
+    #     return(wordlist + " is a palindrome")
+    # else:
+    #     return(wordlist + " is not palindrome")
+    if reversedList == wordlist:
+        return("that is a palindrome")
     else:
-        return(words + " is not palindrome")
+        return("sorry that is not palindrome")
 
-print(ispalindrome(words))
+print(ispalindrome(wordlist))
 
 
 
